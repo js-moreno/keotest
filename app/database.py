e519a7515ac50789364d18d4e6618ff222a8091c
Juan Sebastian Moreno Hernandez <juan.moreno@commity.co>
2022-09-28 20:36:34 -0500

    FIX: DB engine options supported by postgresql

--- app/database.py
+++ app/database.py
@@ -6,9 +6,7 @@ from app.config import get_settings
 
 SQLALCHEMY_DATABASE_URL = get_settings().SQLALCHEMY_DATABASE_URL
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
